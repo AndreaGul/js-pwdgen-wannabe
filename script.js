67c896bd5c5cf0e4cd90a7fba42123f889f6df17
AndreaGul <andreagulino99@gmail.com>
2023-10-31 20:58:27 +0100

    feat: inserimento dati per creare la password

---
+++ script.js
@@ -0,0 +1,11 @@
+const elementNome = document.getElementById('nome');
+const elementCognome = document.getElementById('cognome');
+const elementColore = document.getElementById('colore');
+const elementNumber = document.getElementById('numero');
+
+const number = 7;
+
+elementNome.innerHTML = prompt('inserisci il tuo nome');
+elementCognome.innerHTML = prompt('inserisci il tuo cognome');
+elementColore.innerHTML = prompt('inserisci il tuo colore preferito');
+elementNumber.innerHTML = number;
